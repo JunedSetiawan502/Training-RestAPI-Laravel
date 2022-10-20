9450b38790d8bd662a36f45c44735ea186bb1010
Jnd <70498944+JunedSetiawan@users.noreply.github.com>
2022-10-20 11:11:22 +0700

    add navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Hello, world!</h1>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="student/list">List</a></li>
+        <li><a href="about">About</a></li>
+    </ul>
+    <h1>Home Page</h1>
 @endsection
   
